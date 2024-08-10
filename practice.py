5e76b0310eec34ddf80c3921a5a51cd663fe6bf8
masfana016 <masfanasrullahansari123@gmail.com>
2024-08-10 17:24:20 +0500

    2nd

--- practice.py
+++ practice.py
@@ -1,1 +1,38 @@
-a = 10
+a = int(input ('Enter 1st number: '))
+b = int(input ('Enter 2nd number: '))
+
+result = a + b
+
+print('Sum:', result)
+
+# 2nd Question
+
+side = float(input ('Enter side of square: '))
+
+area = side * side
+
+print('Area of square:', area)
+
+# 3rd Question
+
+a = int(input ('Enter 1st number: '))
+b = int(input ('Enter 2nd number: '))
+
+result = (a + b)/2
+
+print('Average:', result)
+
+# 4th Question
+a = int(input ('Enter 1st number: '))
+b = int(input ('Enter 2nd number: '))
+
+if a>=b:
+    print("True")
+else: print("False")
+
+# 4th Question ANOTHER WAY
+
+a = int(input ('Enter 1st: '))
+b = int(input ('Enter 2nd: '))
+
+print(a>=b)
